d8e636bb4c4b418dff450b2978a5e7631956dbc6
hardyjoysan <hardyjoysan@gmail.com>
2018-03-24 15:17:53 +0530

    Search Products JSON return REST api

--- objects/product.php
+++ objects/product.php
@@ -108,6 +108,25 @@ class Product
 
 		return false;
 	}
+
+	function search($keywords){
+
+		$query = "SELECT c.name as category_name, p.id, p.name, p.description, p.price, p.category_id, p.created FROM " . $this->table_name . " p
+		LEFT JOIN categories c ON p.category_id = c.id WHERE p.name LIKE ? OR p.description LIKE ? OR c.name LIKE ? ORDER BY p.created DESC";
+
+		$stmt = $this->conn->prepare($query);
+
+		$keywords = htmlspecialchars(strip_tags($keywords));
+		$keywords = "%{$keywords}%";
+
+		$stmt->bindParam(1, $keywords);
+		$stmt->bindParam(2, $keywords);
+		$stmt->bindParam(3, $keywords);
+
+		$stmt->execute();
+
+		return $stmt;
+	}
 }
 
 ?>
